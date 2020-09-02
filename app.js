71a5441770595046b5841bcd4b88a375bd45c625
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-09-03 03:20:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,20 +5,45 @@ app.listen(port,()=>{
     console.log(`listening to the port: ${port}`);
 });
 
+//setting view engine
+
+app.set('view engine','ejs');
+
+
 // manage requests
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    const blogs=[
+       {author:"avinash",title:"welcome to node js ", subcategory:"node js",snippet:"hello ! welcome to node js "},
+       {author:"avinash",title:"welcome to express js ", subcategory:"express js",snippet:"hello ! welcome to expres js "},
+       {author:"avinash",title:"welcome to mongo db ", subcategory:"mongo db",snippet:"hello ! welcome to mongodb"},
+       
+       
+
+    ];
+    
+    res.render('index',{title:'HOME',blogs});
+
 });
 
 app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
+    res.render('about',{title:'About Us'});
 });
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'New Blog'});
+})
+
+
+
 
 //404 middleware
 
 app.use((req,res)=>{
-    res.status(400).sendFile('./views/404.html',{root:__dirname});
+
+    res.status(404).render('404',{title:'error | 404'});
+    
+    
 });
 
 
