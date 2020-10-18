de10fb595c2ad7c51cd1e4b95f41b81eb2fac29b
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-10-19 01:40:49 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,29 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const blogSchema=new Schema({
+
+
+    name:{
+        type:String,
+        required:true,
+    },
+    title:
+    {
+        type:String,
+        required:true,
+    },
+    snippet:
+    {
+        type:String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    },
+
+},{timestamps:true});
+
+const Blog=mongoose.model('Blog',blogSchema);
+module.exports=Blog;
